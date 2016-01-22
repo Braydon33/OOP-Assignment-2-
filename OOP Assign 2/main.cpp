f5d864af95a280d6c2589a7ab9f64c7715811632
Braydon Duprey <Braydon@braydons-mbp.oncampus.local>
2016-01-22 08:54:45 -0500

    Initial Commit

---
+++ OOP Assign 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP Assign 2
+//
+//  Created by Braydon Duprey on 2016-01-22.
+//  Copyright © 2016 Braydon Duprey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
